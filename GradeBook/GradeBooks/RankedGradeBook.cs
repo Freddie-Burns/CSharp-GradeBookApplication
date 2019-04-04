1d0202e8ecd55a67519ece2113dcefe16a4c3859
RENISHAW\fb142897 <freddie.burns@renishaw.com>
2019-04-04 09:38:15 +0100

    Created instance of exception.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,9 +13,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
-                throw InvalidOperationException;
-            }
+                throw new InvalidOperationException("Ranked grading requires five or more students");
 
             return 'F';
         }
